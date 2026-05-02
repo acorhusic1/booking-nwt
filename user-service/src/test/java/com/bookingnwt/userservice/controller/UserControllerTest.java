07e66d8c9f01d32cf9f08d572984f3fc9d8a5302
Ahmed Corhusic <acorhusic1@etf.unsa.ba>
2026-05-02 14:50:06 +0200

    Docker containerization for all microservices

--- user-service/src/test/java/com/bookingnwt/userservice/controller/UserControllerTest.java
+++ user-service/src/test/java/com/bookingnwt/userservice/controller/UserControllerTest.java
@@ -7,7 +7,7 @@ import com.bookingnwt.userservice.exception.ResourceNotFoundException;
 import com.bookingnwt.userservice.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
