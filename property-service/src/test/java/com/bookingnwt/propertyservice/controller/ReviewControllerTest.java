d5b4672ca4635a95039ded251e8bff10c61b2feb
Ahmed Čorhusić <148221204+acorhusic1@users.noreply.github.com>
2026-05-02 16:16:12 +0200

    Feature/docker containerization (#2)
    * Add docker containerization for all microservices
    
    * Docker containerization for all microservices
    
    * Remove review-service

--- property-service/src/test/java/com/bookingnwt/propertyservice/controller/ReviewControllerTest.java
+++ property-service/src/test/java/com/bookingnwt/propertyservice/controller/ReviewControllerTest.java
@@ -7,7 +7,7 @@ import com.bookingnwt.propertyservice.exception.ResourceNotFoundException;
 import com.bookingnwt.propertyservice.service.ReviewService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
